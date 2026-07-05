0d9c8242aa7552a14045473dafbe0b70db47eff4
Sina <sinagessner.wuppertal@gmail.com>
2026-07-05 02:05:50 +0200

    Aktualisieren von build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,26 +1,4 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-}
-
-android {
-    namespace = "de.circuitcurios.copyboard"
-    compileSdk = 35
-
-    defaultConfig {
-        applicationId = "de.circuitcurios.copyboard"
-        minSdk = 26
-        targetSdk = 35
-        versionCode = 1
-        versionName = "0.1.0"
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = "17"
-    }
+    id("com.android.application") version "8.7.3" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
 }
